973ae09ef894a8f318821a9e03f9e142e0cdcc6f
Lucero Valdez <lvaldezd.isc91@gmail.com>
2022-08-28 23:44:46 -0500

    new files

--- 03-BaseNodeJs/app.js
+++ 03-BaseNodeJs/app.js
@@ -1,7 +1,11 @@
 
 const {createFile3} = require('./helpers/multiplicar');
 console.clear();
-const base = 7;
+console.log(process.argv);
+const [, , arg3] = process.argv;
+const [,base=0] = arg3.split('=');
+ console.log(base);
+//const base = 7;
 
 createFile3(base)
     .then(nameFile => console.log(nameFile,'creado'))
