6e091971b183f6e4a0ff8d2a2a97c1adddac2758
Lucero Valdez <lvaldezd.isc91@gmail.com>
2022-09-09 00:17:30 -0500

    new functions and actions

--- 04-Task/models/tareas.js
+++ 04-Task/models/tareas.js
@@ -1,8 +1,14 @@
+const Tarea = require("./tarea");
 
 class Tareas {
 
     constructor(_listado){
         this._listado = _listado;
+    }
+
+    crearTareas(desc = ''){
+        const tarea = new Tarea(desc);
+        this._listado[tarea.id] = tarea;
     }
 }
 
