6a095ff02c45e645094e9dd5c0b530f7a104f2bb
Lucero Valdez <lvaldezd.isc91@gmail.com>
2022-09-25 22:44:39 -0500

    add code and class searchs

--- 05-ClimaApp/index.js
+++ 05-ClimaApp/index.js
@@ -1,3 +1,24 @@
 require('colors');
+const {inqMenuOpc,pausa} = require('./helpers/inquirer');
 
-console.log("Hola luz");
+const main = async() => {
+    console.clear();
+    let opts = '';
+
+    do {
+        opts = await inqMenuOpc();
+
+        switch(opts['opc']){
+            case 1:
+
+            break;
+            case 2:
+
+            break;
+        }
+        if(opts['opc']  !== 0) await pausa();
+    } while (opts['opc'] !== 0);
+
+};
+
+main();
