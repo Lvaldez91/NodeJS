3fe9f4317d2a3f686d75dc576471d497248c273e
Lucero Valdez <lvaldezd.isc91@gmail.com>
2022-09-25 22:44:41 -0500

    add new process

--- 05-ClimaApp/helpers/inquirer.js
+++ 05-ClimaApp/helpers/inquirer.js
@@ -7,14 +7,59 @@ const menuOpc = [
         message: 'What do you need to do?',
         choices:[
             {
-                value: '1',
+                value: 1,
                 name: `${'1'.cyan}. Search city`
             },
             {
-                value: '2',
+                value: 2,
                 name: `${'2'.cyan}. History`
+            },
+            {
+                value: 0,
+                name: `${'0'.cyan}. Log Out`
             }
         ]
     }
 ];
 
+const inqMenuOpc = async() => {
+    console.clear();
+    console.log('=========================='.green);
+    console.log('  Seleccione una opción '.blue);
+    console.log('==========================\n'.green);
+
+    const options = await inquirer.prompt(menuOpc);
+    return options;
+};
+
+const pausa = async() => {
+    const question = [{
+        type :  'input',
+        name : 'enter',
+        message: `\nPress ${'ENTER'.green} to continue...\n`
+    }];
+    console.log('\n');
+    await inquirer.prompt(question);
+};
+
+const readInput = async(message) => {
+    const question = [{
+        type : 'input',
+        name : 'desc',
+        message,
+        validate(value) {
+            if(value.length === 0){
+                return 'Please input value';
+            }
+             return true;
+        }
+    }];
+    const {desc} = await inquirer.prompt(question);
+    return desc;
+};
+
+module.exports = {
+    inqMenuOpc,
+    readInput,
+    pausa
+};
