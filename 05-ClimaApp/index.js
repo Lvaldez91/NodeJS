534e166e5253c92b3cbe1ac03e05dd7c10a670fa
Lucero Valdez <lvaldezd.isc91@gmail.com>
2022-09-25 23:00:25 -0500

    add new weather config

--- 05-ClimaApp/index.js
+++ 05-ClimaApp/index.js
@@ -1,16 +1,35 @@
 require('colors');
-const {inqMenuOpc,pausa} = require('./helpers/inquirer');
+const {inqMenuOpc,pausa, readInput} = require('./helpers/inquirer');
+const Searchs = require('./models/searchs');
 
 const main = async() => {
     console.clear();
+
     let opts = '';
+    const search = new Searchs();
 
     do {
         opts = await inqMenuOpc();
 
         switch(opts['opc']){
             case 1:
+                // See message
+                const cityInput = await readInput('City:');
+                console.log(cityInput);
+                // Search citys
 
+                // I can selected city
+
+                // weather
+
+                // I can see results
+                console.log('\nInformation od the city'.green);
+                console.log('City: ',);
+                console.log('Latitude: ',);
+                console.log('Length: ',);
+                console.log('Temperature: ',);
+                console.log('Minimum: ',);
+                console.log('Maximum: ',);
             break;
             case 2:
 
---
+++ 05-ClimaApp/models/searchs.js
@@ -0,0 +1,21 @@
+
+class Searchs {
+
+    historys = ['','','','',''];
+
+    constructor(){
+        // TODO: read DataBase if exists
+
+    }
+
+    async citys(city = '') {
+        // request http
+        console.log(city);
+
+        // return citys
+        return [];
+    }
+}
+
+
+module.exports = Searchs;
