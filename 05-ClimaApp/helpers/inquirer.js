497c375ceb2cfa07506c98232985dfaccb8a53a1
Lucero Valdez <lvaldezd.isc91@gmail.com>
2022-10-01 21:32:19 -0500

    testing

--- 05-ClimaApp/helpers/inquirer.js
+++ 05-ClimaApp/helpers/inquirer.js
@@ -1,65 +1,153 @@
 const inquirer = require('inquirer');
+require('colors');
 
-const menuOpc = [
+const preguntas = [
     {
-        type:'list',
-        name: 'opc',
-        message: 'What do you need to do?',
-        choices:[
+        type: 'list',
+        name: 'opcion',
+        message: '¿Qué desea hacer?',
+        choices: [
             {
                 value: 1,
-                name: `${'1'.cyan}. Search city`
+                name: `${ '1.'.green } Buscar ciudad`
             },
             {
                 value: 2,
-                name: `${'2'.cyan}. History`
+                name: `${ '2.'.green } Historial`
             },
             {
                 value: 0,
-                name: `${'0'.cyan}. Log Out`
-            }
+                name: `${ '0.'.green } Salir`
+            },
         ]
     }
 ];
 
-const inqMenuOpc = async() => {
+
+
+const inquirerMenu = async() => {
+
     console.clear();
     console.log('=========================='.green);
-    console.log('  Seleccione una opción '.blue);
+    console.log('  Seleccione una opción'.white );
     console.log('==========================\n'.green);
 
-    const options = await inquirer.prompt(menuOpc);
-    return options;
-};
+    const { opcion } = await inquirer.prompt(preguntas);
+
+    return opcion;
+}
 
 const pausa = async() => {
-    const question = [{
-        type :  'input',
-        name : 'enter',
-        message: `\nPress ${'ENTER'.green} to continue...\n`
-    }];
+    
+    const question = [
+        {
+            type: 'input',
+            name: 'enter',
+            message: `Presione ${ 'enter'.green } para continuar`
+        }
+    ];
+
     console.log('\n');
     await inquirer.prompt(question);
-};
+}
 
-const readInput = async(message) => {
-    const question = [{
-        type : 'input',
-        name : 'desc',
-        message,
-        validate(value) {
-            if(value.length === 0){
-                return 'Please input value';
+const leerInput = async( message ) => {
+
+    const question = [
+        {
+            type: 'input',
+            name: 'desc',
+            message,
+            validate( value ) {
+                if( value.length === 0 ) {
+                    return 'Por favor ingrese un valor';
+                }
+                return true;
             }
-             return true;
         }
-    }];
-    const {desc} = await inquirer.prompt(question);
+    ];
+
+    const { desc } = await inquirer.prompt(question);
     return desc;
-};
+}
+
+const listarLugares = async( lugares = [] ) => {
+
+    const choices = lugares.map( (lugar, i) => {
+
+        const idx = `${i + 1}.`.green;
+
+        return {
+            value: lugar.id,
+            name:  `${ idx } ${ lugar.nombre }`
+        }
+    });
+
+    choices.unshift({
+        value: '0',
+        name: '0.'.green + ' Cancelar'
+    });
+
+    const preguntas = [
+        {
+            type: 'list',
+            name: 'id',
+            message: 'Seleccione lugar:',
+            choices
+        }
+    ]
+
+    const { id } = await inquirer.prompt(preguntas);
+    return id;
+}
+
+const confirmar = async(message) => {
+
+    const question = [
+        {
+            type: 'confirm',
+            name: 'ok',
+            message
+        }
+    ];
+
+    const { ok } = await inquirer.prompt(question);
+    return ok;
+}   
+
+const mostrarListadoChecklist = async( tareas = [] ) => {
+
+    const choices = tareas.map( (tarea, i) => {
+
+        const idx = `${i + 1}.`.green;
+
+        return {
+            value: tarea.id,
+            name:  `${ idx } ${ tarea.desc }`,
+            checked: ( tarea.completadoEn ) ? true : false
+        }
+    });
+
+    const pregunta = [
+        {
+            type: 'checkbox',
+            name: 'ids',
+            message: 'Selecciones',
+            choices
+        }
+    ]
+
+    const { ids } = await inquirer.prompt(pregunta);
+    return ids;
+}
+
+
 
 module.exports = {
-    inqMenuOpc,
-    readInput,
-    pausa
-};
+    inquirerMenu,
+    pausa,
+    leerInput,
+    listarLugares,
+    confirmar,
+    mostrarListadoChecklist
+}
