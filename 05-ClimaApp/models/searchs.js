bf92e2f862edd4904ce5afe56eab85f04e50f7ee
Lucero Valdez <lvaldezd.isc91@gmail.com>
2022-10-01 20:56:11 -0500

    add environment variables with process.env

--- 05-ClimaApp/models/searchs.js
+++ 05-ClimaApp/models/searchs.js
@@ -5,20 +5,33 @@ class Searchs {
 
     constructor(){
         // TODO: read DataBase if exists
+    }
 
+    get paramsMapBox(){
+        return{
+            //'access_token': '',
+            'limit': '5',
+            'language':'es'
+        };
     }
 
     async citys(city = '') {
         try{
             // request http
-            const resp = await axios.get('https://reqres.in/api/users?page=2');
-            console.log(resp.data);
-
+            //const resp = await axios.get('https://reqres.in/api/users?page=2');
+            //console.log(resp.data);
             // return citys
-        } catch(error){
+            const instance = axios.create({
+                baseURL:  `https://reqres.in/api/users?page=2`,
+                params: this.paramsMapBox
+            });
 
+            const resp = await instance.get();
+            console.log(resp.data);
+            return [];
+        } catch(error){
+            console.log('Error: ' + error);
         }
-        
         return [];
     }
 }
