f75d68cb3aefb15d6c9ede6728fb7c27203d35b7
Lucero Valdez <lvaldezd.isc91@gmail.com>
2022-12-11 13:37:42 -0600

    services with modify header

--- 06-WebService/app.js
+++ 06-WebService/app.js
@@ -1,25 +1,8 @@
+const express = require('express');
+const app = express();
 
-// documentation: https://nodejs.org/docs/latest-v17.x/api/https.html#httpscreateserveroptions-requestlistener
-const http = require('http');
+app.get('/', function (req, res) {
+  res.send('Hello World');
+});
 
-// callback: solicitud/responde el server
-http.createServer((request, response) => {
-    console.log(request);
-    // declara un text-plain: response.writeHead(402,{'Content-Type':'text/plain'});
-    // declara un json en el header
-    response.writeHead(402,{'Content-Type':'application/json'});
-
-    const persona = {
-        id:1,
-        name:'Luz Valdez'
-    };
-    response.write(JSON.stringify(persona));
-    //response.write('404 | Page not found');
-    //response.write('Hello World');
-    response.end();
-}).listen(8080);
-
-
-console.log('Listen to port' , 8080);
-
-
+app.listen(8080);
