c5b5576c3720b58290d37980d9074f3d672d262b
Lucero Valdez <lvaldezd.isc91@gmail.com>
2022-12-11 17:18:04 -0600

    services with pages and service

--- 06-WebService/app.js
+++ 06-WebService/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+
+//Middleware - contenido estatico
+app.use(express.static('public'));
+
+
 app.get('/', function (req, res) {
     res.send('Home Page');
 });
@@ -10,7 +15,7 @@ app.get('/hello-world', function (req, res) {
 });
 
 app.get('*', function (req, res) {
-    res.send('404 | Page not found');
+    res.sendFile(__dirname + '/public/404.html');
 });
 
 app.get(port, () =>{
