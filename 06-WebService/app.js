04db47746dfb2f1d2b21e9a6074b3f2805a95a06
Lucero Valdez <lvaldezd.isc91@gmail.com>
2022-12-11 13:44:52 -0600

    services with Express

--- 06-WebService/app.js
+++ 06-WebService/app.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
-
+const port = 8080;
 app.get('/', function (req, res) {
-  res.send('Hello World');
+    res.send('Home Page');
 });
 
-app.listen(8080);
+app.get('/hello-world', function (req, res) {
+    res.send('Hello World with root');
+});
+
+app.get('*', function (req, res) {
+    res.send('404 | Page not found');
+});
+
+app.get(port, () =>{
+    console.log(`Example app listenin at http://localhost:${port}`);
+});
+
+app.listen(port);
