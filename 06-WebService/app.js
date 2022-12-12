5f46f5a4365ee788eee19034efccceae845a1431
Lucero Valdez <lvaldezd.isc91@gmail.com>
2022-12-12 00:30:03 -0600

    services with pages and service 2

--- 06-WebService/app.js
+++ 06-WebService/app.js
@@ -7,12 +7,17 @@ app.use(express.static('public'));
 
 
 app.get('/', function (req, res) {
-    res.send('Home Page');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
-app.get('/hello-world', function (req, res) {
-    res.send('Hello World with root');
+app.get('/generic', function (req, res) {
+    res.sendFile(__dirname + '/public/generic.html');
 });
+
+app.get('/elements', function (req, res) {
+    res.sendFile(__dirname + '/public/elements.html');
+});
+
 
 app.get('*', function (req, res) {
     res.sendFile(__dirname + '/public/404.html');
